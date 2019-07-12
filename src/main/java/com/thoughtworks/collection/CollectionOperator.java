08f3bbb9c09917788134c3a729531854cc39defd
REET CHEN (ITA-ISDC-ISD-OOCLL/ZHA) <CHENRE6@OOCL.COM>
2019-07-12 17:32:12 +0800

    implement and test the function named getListByInterval in CollectionOperator.

--- src/main/java/com/thoughtworks/collection/CollectionOperator.java
+++ src/main/java/com/thoughtworks/collection/CollectionOperator.java
@@ -2,11 +2,17 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class CollectionOperator {
     public List<Integer> getListByInterval(int left, int right) {
-        throw new NotImplementedException();
+        //throw new NotImplementedException();
+
+       return IntStream.rangeClosed(left,right).boxed().collect(Collectors.toList());
+       
     }
 
     public List<Integer> getEvenListByIntervals(int left, int right) {
