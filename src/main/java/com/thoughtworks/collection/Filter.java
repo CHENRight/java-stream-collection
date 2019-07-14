8483e4f501e342786a9f690cff16f391df554450
REET CHEN (ITA-ISDC-ISD-OOCLL/ZHA) <CHENRE6@OOCL.COM>
2019-07-14 16:03:37 +0800

    implement and test the function of Filter.java

--- src/main/java/com/thoughtworks/collection/Filter.java
+++ src/main/java/com/thoughtworks/collection/Filter.java
@@ -2,8 +2,10 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Filter {
 
@@ -14,18 +16,23 @@ public class Filter {
     }
 
     public List<Integer> filterEven() {
-        throw new NotImplementedException();
+        //throw new NotImplementedException();
+        return  array.stream().filter(i -> i % 2 == 0).collect(Collectors.toList());
     }
 
     public List<Integer> filterMultipleOfThree() {
-        throw new NotImplementedException();
-    }
-
-    public List<Integer> getCommonElements(List<Integer> firstList, List<Integer> secondList) {
-        throw new NotImplementedException();
+        //throw new NotImplementedException();
+        return array.stream().filter(i -> i % 3 == 0).collect(Collectors.toList());
     }
 
     public List<Integer> getDifferentElements() {
-        throw new NotImplementedException();
+//        throw new NotImplementedException();
+        return array.stream().distinct().collect(Collectors.toList());
     }
+
+    public List<Integer> getCommonElements(List<Integer> firstList, List<Integer> secondList) {
+//        throw new NotImplementedException();
+        return  firstList.stream().filter(i -> secondList.contains(i)).collect(Collectors.toList());
+    }
+
 }
